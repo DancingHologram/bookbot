42c416c94004f2e207c6c8a93195b4e6994e7a4b
Hologram <cngolden3@gmail.com>
2025-10-23 15:57:20 -0700

    added stats.py and changed main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-def get_books():
-    path_to_file = ("books/frankenstein.txt")
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        return file_contents
-def word_count():
-    book_words = get_books()
-    book_word_count = book_words.split()
-    total_words = len(book_word_count)
-    return total_words
+from stats import get_books
+from stats import word_count
 
 def main():
     book_text = word_count()
