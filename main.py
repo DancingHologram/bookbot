981fc448c981bc065c1b2e72393eb789bc6b0e70
Hologram <cngolden3@gmail.com>
2025-10-23 15:52:30 -0700

    changed main.py to count words in a .txt file

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-def get_books_text():
+def get_books():
     path_to_file = ("books/frankenstein.txt")
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
+def word_count():
+    book_words = get_books()
+    book_word_count = book_words.split()
+    total_words = len(book_word_count)
+    return total_words
 
 def main():
-    book_text = get_books_text()
-    print(book_text)
+    book_text = word_count()
+    print(f"Found {book_text} total words")
 
 main()
