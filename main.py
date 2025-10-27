aa21cc7b0e6afa218d308e9e10f087ed37612c0d
Hologram <cngolden3@gmail.com>
2025-10-26 21:54:30 -0700

    Added sys module. Takes path as argument and uses the program to print character and word count.

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from stats import word_count, sorted_list
+import sys
+
+len(sys.argv)
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+path = sys.argv[1]
 
 def main():
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}")
     print("----------- Word Count ----------")
-    print(f"Found {word_count()} total words")
+    print(f"Found {word_count(path)} total words")
     print("--------- Character Count -------")
-    for item in sorted_list():
+    for item in sorted_list(path):
         ch = item["char"]
         if ch.isalpha():
             print(f"{ch}: {item['num']}")
--- stats.py
+++ stats.py
@@ -1,18 +1,17 @@
 def sort_on(items):
     return items["num"]
-def get_books():
-    path_to_file = ("books/frankenstein.txt")
-    with open(path_to_file) as f:
+def get_books(path):
+    with open(path, "r", encoding="utf-8") as f:
         file_contents = f.read()
         return file_contents
-def word_count():
-    book_words = get_books()
+def word_count(path):
+    book_words = get_books(path)
     book_word_count = book_words.split()
     total_words = len(book_word_count)
     return total_words
-def character_count():
+def character_count(path):
    character_counts = {}
-   book_words = get_books()
+   book_words = get_books(path)
    char_occurence = book_words.lower()
    for c in char_occurence:
         if c.isalpha():
@@ -21,9 +20,9 @@ def character_count():
             else:
                 character_counts[c] += 1
    return character_counts
-def sorted_list():
+def sorted_list(path):
     unsorted_list = []
-    unsorted_dict = character_count()
+    unsorted_dict = character_count(path)
     for ch, cnt in unsorted_dict.items():
         unsorted_list.append({"char":ch, "num": cnt})
     unsorted_list.sort(reverse=True, key=sort_on)
